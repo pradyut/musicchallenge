25da4fbdc5eb180ce28d4e3774f65ae75394d499
Pradyut Pokuri <pradyut2386@gmail.com>
2014-03-30 20:04:58 -0400

    Search functionality, search results display, artist albums search and display, album tracks search and display

--- public/js/app/MainController.js
+++ public/js/app/MainController.js
@@ -1,37 +1,112 @@
 var app = angular.module('MusicChallenge', ["ui.bootstrap", "ui.router"]);
 app.config(function($stateProvider, $urlRouterProvider){
       
-      // For any unmatched url, send to /route1
+  // For any unmatched url, send to /index
   $urlRouterProvider.otherwise("/index");
   
   $stateProvider
     .state('index', {
-        url: "/index",
-        templateUrl: "partials/main.html"
+      url: "/index",
+      templateUrl: "partials/main.html"
     })
-      .state('search', {
-          url: "/search",
-          templateUrl: "partials/search.html",
-          controller: function($scope){
-            
-          }
-      })
-      
-    .state('route2', {
-        url: "/route2",
-        templateUrl: "route2.html"
+    .state('search', {
+      url: "/search",
+      templateUrl: "partials/search.html",
+      controller: function($scope){
+          
+      }
     })
-      .state('route2.list', {
-          url: "/list",
-          templateUrl: "route2.list.html",
-          controller: function($scope){
-            $scope.things = ["A", "Set", "Of", "Things"];
-          }
-      });
+    .state('search.details', {
+      url: "/:term/{page}",
+      templateUrl: "partials/search.details.html",
+      controller: 'SearchController'
+    })
+    .state('artist', {
+      url: "/artist",
+      templateUrl: "partials/artist.html",
+      controller: function($scope){
+          
+      }
+    })
+    .state('artist.details', {
+      url: "/:name/:id/",
+      templateUrl: "partials/artist.details.html",
+      controller: 'ArtistController'
+    })
+    .state('album', {
+      url: "/album",
+      templateUrl: "partials/album.html",
+      controller: function($scope){
+          
+      }
+    })
+    .state('album.details', {
+      url: "/:name/:id/",
+      templateUrl: "partials/album.details.html",
+      controller: 'AlbumController'
+    });
   
 });
 app.controller("MainController", function($scope, $state){
   $scope.search = function() {
-    $state.go('search')
+    $state.go('search.details',{'term': $scope.term });
   };
+});
+
+app.controller("SearchController", function($scope, $http, $stateParams){
+  $scope.term = $stateParams.term;
+  $scope.page = $stateParams.page;
+  
+  $scope.getNext = function() {
+    return location.hash+(parseInt($scope.current)+1);
+  };
+  url = 'http://ws.spotify.com/search/1/artist.json?q='+$scope.term;
+  if($scope.page)
+    url += "&page=" + $scope.page;
+  $http({method: 'GET', url: url}).
+    success(function(data, status, headers, config) {
+      $scope.loaded = true;
+      $scope.total = data.info.num_results;
+      $scope.current = data.info.page;
+      $scope.artists = data.artists;
+    }).
+    error(function(data, status, headers, config) {
+      
+    });
+});
+
+
+app.controller("ArtistController", function($scope, $http, $stateParams){
+  $scope.id = $stateParams.id;
+  $scope.name = $stateParams.name;
+  
+  
+  url = 'http://ws.spotify.com/lookup/1/.json?uri='+$scope.id+'&extras=albumdetail';
+  
+  $http({method: 'GET', url: url}).
+    success(function(data, status, headers, config) {
+      $scope.loaded = true;
+      $scope.albums = data.artist.albums;
+    }).
+    error(function(data, status, headers, config) {
+      
+    });
+});
+
+
+app.controller("AlbumController", function($scope, $http, $stateParams){
+  $scope.id = $stateParams.id;
+  $scope.name = $stateParams.name;
+  
+  
+  url = 'http://ws.spotify.com/lookup/1/.json?uri='+$scope.id+'&extras=trackdetail';
+  
+  $http({method: 'GET', url: url}).
+    success(function(data, status, headers, config) {
+      $scope.loaded = true;
+      $scope.tracks = data.album.tracks;
+    }).
+    error(function(data, status, headers, config) {
+      
+    });
 });
